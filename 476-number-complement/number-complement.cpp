3e93a6ed3cd2171f989e9cc64449a9464df4a46a
KabileshPS <146936263+KabileshPS@users.noreply.github.com>
2026-01-06 12:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.51%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findComplement(int num) {
+       int n=num;
+       int c=0;
+       long long r=0;
+       long long power=1;
+       while(n!=0)
+       {
+           c=n%2;
+           if(c==0)
+           c=1;
+           else if (c==1)
+           c=0;
+           if(c==1)
+           r=r+power;
+           power=power*2;
+           n=n/2;
+       }
+       return (int)r;
+    }
+};
